e361d182689a7bea67feb914cdf5e682f9ca72c9
YeJieChen <1940879828@qq.com>
2026-07-11 21:38:00 +0800

    feat: 格式化代码

--- app/src/main/java/com/example/myapplication/features/home/HomeScreen.kt
+++ app/src/main/java/com/example/myapplication/features/home/HomeScreen.kt
@@ -9,8 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
